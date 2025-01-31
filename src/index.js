66f862d2ae451609eef0a3cbe813859c803b023c
Ray Lopez <raylopez509@gmail.com>
2025-01-30 23:29:41 -0800

    created task class and created function for info Created Task class and created a function for entering the info. It works in the console. To do:   - add event to button to add an event   - add task doms to page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './index.css';
 
 console.log('hi');
+
+class Task {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+function enterTaskInformation() {
+  let title = prompt('Enter task title');
+  let description = prompt('Enter task description');
+  let dueDate = prompt('When is it due?');
+  let priority = prompt('what is the priority');
+  let task = new Task(title, description, dueDate, priority);
+  return task;
+}
+
+console.log(enterTaskInformation());
