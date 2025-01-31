ab04b2af0ac838e9dc602845d6848a6efc31016b
Ray Lopez <raylopez509@gmail.com>
2025-01-30 22:25:21 -0800

    added base index/css file, added to webpack config also added the webpack package for html and css installed webpack server too

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("hi");
+import './index.css';
+
+console.log('hi');
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
