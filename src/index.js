83ceb0510a93b12a8c6bb6134324a182152c91e3
Ray Lopez <raylopez509@gmail.com>
2025-01-31 11:55:38 -0800

    put add task code into function Adding it into function will allow deleting and creating tasks to be easier. Also trying to make it so we can hide and show certain parts of the task

--- src/index.js
+++ src/index.js
@@ -8,23 +8,26 @@ class Task {
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.id = undefined;
   }
 }
 
-function enterTaskInformation() {
-  let title = prompt('Enter task title');
-  let description = prompt('Enter task description');
-  let dueDate = prompt('When is it due?');
-  let priority = prompt('what is the priority');
-  let task = new Task(title, description, dueDate, priority);
-  return task;
-}
+// function enterTaskInformation() {
+//   let title = prompt('Enter task title');
+//   let description = prompt('Enter task description');
+//   let dueDate = prompt('When is it due?');
+//   let priority = prompt('what is the priority');
+//   let task = new Task(title, description, dueDate, priority);
+//   return task;
+// }
+
+// createTaskDOM(enterTaskInformation());
 
 function createTaskDOM(task) {
   let taskSection = document.createElement("section");
   let taskContainer = document.querySelector(".task-container");
   taskSection.appendChild(createDOMElement("p",task.title));
-  taskSection.appendChild(createDOMElement("p",task.description));
+  taskSection.appendChild(createDOMElement("p",task.description).);
   taskSection.appendChild(createDOMElement("p",task.dueDate));
   taskSection.appendChild(createDOMElement("p",task.priority));
   taskContainer.appendChild(taskSection);
@@ -36,11 +39,20 @@ function createDOMElement(tag, text) {
   return element;
 }
 
-// createTaskDOM(enterTaskInformation());
+let testTask = new Task("test1","test2","test3","test4")
 
 const addTaskButton = document.querySelector(".add-task-button");
-addTaskButton.addEventListener("click", () => createTaskDOM(new Task("test1","test2","test3","test4")));
+addTaskButton.addEventListener("click", () => createTaskDOM(testTask));
 
-createTaskDOM(new Task("test1","test2","test3","test4"));
+let tasks = [];
 
+function addTask(task) {
+  task.id = tasks.length;
+  tasks.push(task);
+  createTaskDOM(task);
+}
+
+addTask(testTask);
+
+console.log(tasks);
 
